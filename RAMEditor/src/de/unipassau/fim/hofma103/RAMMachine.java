4b7b2868c4256b509ba59e694b01582f9aac09b3
hofma103 <hofma103@users.noreply.github.com>
2017-05-04 19:27:39 +0200

    reformatting changed the output of processCode to show the proper function calls (might be removed if I get some better way to show which part is executed at the moment)

--- RAMEditor/src/de/unipassau/fim/hofma103/RAMMachine.java
+++ RAMEditor/src/de/unipassau/fim/hofma103/RAMMachine.java
@@ -10,16 +10,16 @@ public class RAMMachine {
 	private Memory memory;
 	private ArrayList<String> functions;
 	private ArrayList<Integer> functionParameters;
-	
+
 	private int accumulator = 0;
 	private int programCounter = 0;
-	
+
 	public RAMMachine(int programLength) {
 		memory = new Memory();
 		functions = new ArrayList<String>(programLength);
 		functionParameters = new ArrayList<Integer>(programLength);
 	}
-	
+
 	public void inputCode(ArrayList<String> code) {
 		for (int i = 0; i < code.size(); i++) {
 			String str = code.get(i);
@@ -37,20 +37,26 @@ public class RAMMachine {
 			functionParameters.add(methodParam);
 		}
 	}
-	
+
 	public void processCode() {
 		while (programCounter < functions.size()) {
-			System.out.println(functions.get(programCounter) + functionParameters.get(programCounter));
+			String function = functions.get(programCounter);
+			int functionParameter = functionParameters.get(programCounter);
+			if (function.equals("read") || function.equals("print"))
+				System.out.println(String.format("%s()", function));
+			else
+				System.out.println(
+						String.format("%s(%d)", function.replace("At", "@"), functionParameter));
 			java.lang.reflect.Method method = null;
 			try {
-				method = this.getClass().getMethod(functions.get(programCounter), int.class);
+				method = this.getClass().getMethod(function, int.class);
 			} catch (NoSuchMethodException e1) {
 				e1.printStackTrace();
 			} catch (SecurityException e1) {
 				e1.printStackTrace();
 			}
 			try {
-				method.invoke(this, functionParameters.get(programCounter));
+				method.invoke(this, functionParameter);
 			} catch (IllegalAccessException e) {
 				e.printStackTrace();
 			} catch (IllegalArgumentException e) {
@@ -61,90 +67,105 @@ public class RAMMachine {
 			programCounter++;
 		}
 	}
-	
+
 	public void add(int param) {
 		accumulator = accumulator + memory.get(param);
 	}
+
 	public void addAt(int param) {
 		accumulator = accumulator + memory.get(memory.get(param));
 	}
+
 	public void addAbs(int param) {
 		accumulator = accumulator + param;
 	}
-	
+
 	public void sub(int param) {
 		accumulator = accumulator - memory.get(param);
 	}
+
 	public void subAt(int param) {
 		accumulator = accumulator - memory.get(memory.get(param));
 	}
+
 	public void subAbs(int param) {
 		accumulator = accumulator - param;
 	}
-	
+
 	public void mult(int param) {
 		accumulator = accumulator * memory.get(param);
-	}	
-	public void multAt(int param) {
-		accumulator = accumulator * memory.get(memory.get(param));		
 	}
+
+	public void multAt(int param) {
+		accumulator = accumulator * memory.get(memory.get(param));
+	}
+
 	public void multAbs(int param) {
 		accumulator = accumulator * param;
 	}
-	
+
 	public void div(int param) {
 		accumulator = accumulator / memory.get(param);
 	}
+
 	public void divAt(int param) {
 		accumulator = accumulator / memory.get(memory.get(param));
 	}
+
 	public void divAbs(int param) {
 		accumulator = accumulator / param;
 	}
-	
+
 	public void load(int param) {
 		accumulator = memory.get(param);
 	}
+
 	public void loadAt(int param) {
 		accumulator = memory.get(memory.get(param));
 	}
+
 	public void loadAbs(int param) {
 		accumulator = param;
 	}
-	
+
 	public void store(int param) {
 		memory.set(param, accumulator);
 	}
+
 	public void storeAt(int param) {
 		memory.set(memory.get(param), accumulator);
 	}
-	
+
 	public void jumpGtz(int param) {
 		if (accumulator > 0)
 			programCounter = memory.get(param) - 2;
 	}
+
 	public void jumpGtzAt(int param) {
 		if (accumulator > 0)
 			programCounter = memory.get(memory.get(param)) - 2;
 	}
+
 	public void jumpGtzAbs(int param) {
 		if (accumulator > 0)
 			programCounter = param - 2;
 	}
-	
+
 	public void jumpZ(int param) {
 		if (accumulator == 0)
 			programCounter = memory.get(param) - 2;
 	}
+
 	public void jumpZAt(int param) {
 		if (accumulator == 0)
 			programCounter = memory.get(memory.get(param)) - 2;
 	}
+
 	public void jumpZAbs(int param) {
 		if (accumulator == 0)
 			programCounter = param - 2;
 	}
-	
+
 	public void read(int param) {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		try {
@@ -154,7 +175,7 @@ public class RAMMachine {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void print(int param) {
 		System.out.println(accumulator);
 	}
