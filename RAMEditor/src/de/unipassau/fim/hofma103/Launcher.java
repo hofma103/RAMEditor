2d15da24cbaa34695b2a3998e3f498a182d61e8d
hofma103 <hofma103@users.noreply.github.com>
2017-05-05 23:24:04 +0200

    eventqueue variable for sending awtevents to the gui

--- RAMEditor/src/de/unipassau/fim/hofma103/Launcher.java
+++ RAMEditor/src/de/unipassau/fim/hofma103/Launcher.java
@@ -1,15 +1,21 @@
 package de.unipassau.fim.hofma103;
 
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
 import javax.swing.text.BadLocationException;
 
 public class Launcher {
 	public static boolean enableMemDump = false;
+	public static EventQueue queue;
 
 	public static void main(String[] args) {
 		if (args.length > 0) {
 			if (args[0].equalsIgnoreCase("enableMemDump"))
 				enableMemDump = true;
 		}
+		
+		queue = Toolkit.getDefaultToolkit().getSystemEventQueue();
 
 		EditorPanel editor = new EditorPanel();
 		try {
