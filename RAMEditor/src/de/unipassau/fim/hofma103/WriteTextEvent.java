68fbacc3b55e78b8c387da4281ff473c0476f932
hofma103 <hofma103@users.noreply.github.com>
2017-05-05 23:08:54 +0200

    AWTEvent to synch the console window with output from the program (swing is not threadsafe and can result in random deadlocks when trying to modify the window components from a different thread)

---
+++ RAMEditor/src/de/unipassau/fim/hofma103/WriteTextEvent.java
@@ -0,0 +1,23 @@
+package de.unipassau.fim.hofma103;
+
+import java.awt.AWTEvent;
+
+@SuppressWarnings("serial")
+public class WriteTextEvent extends AWTEvent {
+	public static final int id = AWTEvent.RESERVED_ID_MAX + 1;
+	private String str;
+
+	public WriteTextEvent(Object target, String str) {
+		super(target, id);
+		this.str = str;
+	}
+
+	public String getContent() {
+		return str;
+	}
+	
+	@Override
+	public int getID() {
+		return id;
+	}
+}
