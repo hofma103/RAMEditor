6e4f8fc8290dcb202a758a39d75b38762c3d0cd2
hofma103 <hofma103@users.noreply.github.com>
2017-05-05 17:56:48 +0200

    added commandline switch for memory dump

--- RAMEditor/src/de/unipassau/fim/hofma103/Launcher.java
+++ RAMEditor/src/de/unipassau/fim/hofma103/Launcher.java
@@ -3,7 +3,14 @@ package de.unipassau.fim.hofma103;
 import javax.swing.text.BadLocationException;
 
 public class Launcher {
+	public static boolean enableMemDump = false;
+
 	public static void main(String[] args) {
+		if (args.length > 0) {
+			if (args[0].equalsIgnoreCase("enableMemDump"))
+				enableMemDump = true;
+		}
+
 		EditorPanel editor = new EditorPanel();
 		try {
 			editor.showEditorPanel();
